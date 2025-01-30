e2187aa9053a210a54e386299df50ee2d700a72f
Jeremiah Butler <git@jbutler.dev>
2025-01-30 16:28:57 -0500

    updates

--- model.go
+++ model.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"genai/tools"
 
@@ -41,11 +42,16 @@ func (m *Model) AddTool(toolsToAdd ...*tools.Tool) error {
 }
 
 func (m *Model) generate(prompt string) (string, error) {
-	resp, err := m.Gemini.GenerateContent(context.Background(), gemini.Text(prompt))
+	input := &retryableGeminiCallInput{
+		ctx:   context.Background(),
+		model: m,
+		part:  gemini.Text(prompt),
+	}
+	resp, err := retryableGeminiCall(input, 0, 1*time.Second)
 	if err != nil {
 		return "", fmt.Errorf("failed to generate content: %v", err)
 	}
-	return handleText(resp), nil
+	return handleGeminiText(resp), nil
 }
 
 func (m *Model) chat(ctx context.Context, chat *Chat) error {
@@ -56,11 +62,17 @@ func (m *Model) chat(ctx context.Context, chat *Chat) error {
 			if DEBUG {
 				log.Println("Sending message", msg)
 			}
-			res, err := m.geminiSession.SendMessage(ctx, gemini.Text(msg))
+			input := &retryableGeminiCallInput{
+				ctx:     ctx,
+				model:   m,
+				session: m.geminiSession,
+				part:    gemini.Text(msg),
+			}
+			res, err := retryableGeminiCall(input, 0, 1*time.Second)
 			if err != nil {
 				log.Println("Failed to send message", err)
 			}
-			err = handleResponse(m, chat, res)
+			err = handleGeminiResponse(m, chat, res)
 			if err != nil {
 				log.Println("Failed to handle response", err)
 			}
@@ -68,63 +80,4 @@ func (m *Model) chat(ctx context.Context, chat *Chat) error {
 			return nil
 		}
 	}
-}
-
-func handleResponse(m *Model, chat *Chat, resp *gemini.GenerateContentResponse) error {
-	for _, cand := range resp.Candidates {
-		if cand.Content != nil {
-			for _, part := range cand.Content.Parts {
-				switch p := part.(type) {
-				case gemini.FunctionCall:
-					if DEBUG {
-						log.Println("Handling function call", p)
-					}
-					resp, err := handleFunctionCall(m, &p)
-					if err != nil {
-						return fmt.Errorf("failed to handle function call: %v", err)
-					}
-					if resp == nil {
-						return nil
-					}
-					mresp, err := m.geminiSession.SendMessage(chat.ctx, resp)
-					if err != nil {
-						return fmt.Errorf("failed to send message: %v", err)
-					}
-					return handleResponse(m, chat, mresp)
-				case gemini.Text:
-					if DEBUG {
-						log.Println("Handling text", part)
-					}
-					chat.Recv <- fmt.Sprintf("%v", part)
-				default:
-					return fmt.Errorf("unexpected part: %v", part)
-				}
-			}
-		}
-	}
-	return nil
-}
-
-func handleFunctionCall(m *Model, f *gemini.FunctionCall) (gemini.Part, error) {
-	resp, err := m.Provider.RunTool(f.Name, f.Args)
-	if err != nil {
-		return nil, fmt.Errorf("failed to run tool: %v", err)
-	}
-	part, ok := resp.(gemini.FunctionResponse)
-	if !ok {
-		return nil, fmt.Errorf("unexpected response type: %v", resp)
-	}
-	return part, nil
-}
-
-func handleText(resp *gemini.GenerateContentResponse) string {
-	var text string
-	for _, cand := range resp.Candidates {
-		if cand.Content != nil {
-			for _, part := range cand.Content.Parts {
-				text += fmt.Sprintf("%v", part)
-			}
-		}
-	}
-	return text
 }
